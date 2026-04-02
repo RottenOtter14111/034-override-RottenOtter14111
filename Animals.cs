784127c3b40cd104eaeb0cf9c439793190523b64
RottenOtter14111 <arogers@anamosa.k12.ia.us>
2026-04-02 17:20:37 +0000

    wooahhh

--- Animals.cs
+++ Animals.cs
@@ -2,6 +2,13 @@
 
 public abstract class Animal {
    public abstract string Speak();
+
+   public class Cat : Animal
+   {
+    public override string Speak()
+      {
+         return "Meow!";
+      }
+   }
 }
 
-// Add your code here
